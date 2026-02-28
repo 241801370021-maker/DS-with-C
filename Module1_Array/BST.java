4f956f1d8ab08608ca29080b5b6c0cb82c4c3854
241801370021-maker <241801370021@cutmap.ac.in>
2026-02-28 15:15:41 +0530

    Refactor BST methods and improve structure

--- Module1_Array/BST.java
+++ Module1_Array/BST.java
@@ -1,47 +1,54 @@
-// Online Java Compiler
-class BST{
+class BST {
     Node root;
-    
-    // SEARCH METHOD
-    boolean searchinBST(int key, Node root){
+
+    boolean searchinBST(int key, Node root) {
         Node curr = root;
-        while(curr != null){
-            if(key == curr.d){
+        while (curr != null) {
+            if (key == curr.d) {
                 return true;
-            } 
-            else if(key > curr.d){
+            } else if (key > curr.d) {
                 curr = curr.r;
-            } 
-            else{
+            } else {
                 curr = curr.l;
             }
         }
         return false;
     }
+
+    void insertinBST(int key, Node root) {
+        Node curr = root;
+        Node p = null;
+        while (curr != null) {
+            p = curr;
+
+            if (key > curr.d) {
+                curr = curr.r;
+            } else {
+                curr = curr.l;
+            }
+        }
+        Node nn = new Node(key);
+        if (p.d < key) {
+            p.r = nn;
+        } else {
+            p.l = nn;
+        }
+    }
+
     
-    // INSERT METHOD
-    Node insert(Node root, int key){
-        
-        if(root == null){
-            return new Node(key);
+    void inorder(Node root) {
+        if (root != null) {
+            inorder(root.l);   // left
+            System.out.print(root.d + " ");  // root
+            inorder(root.r);   // right
         }
-        
-        if(key < root.d){
-            root.l = insert(root.l, key);
-        }
-        else if(key > root.d){
-            root.r = insert(root.r, key);
-        }
-        
-        return root;
     }
 }
 
-class Node{
+class Node {
     int d;
     Node l, r;
-    
-    Node(int d){
+    Node(int d) {
         this.d = d;
         this.l = this.r = null;
     }
@@ -49,10 +56,11 @@ class Node{
 
 class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Try programiz.pro");
+
         BST t = new BST();
-        
-        Node a = new Node(40);
+
+        Node a = new Node(40);   // root
         Node b = new Node(20);
         Node c = new Node(60);
         Node d = new Node(15);
@@ -60,20 +68,20 @@ class Main {
         Node f = new Node(50);
         Node g = new Node(70);
         Node h = new Node(10);
-        
+
         // linking
-        a.l = b;
+        a.l = b; // root
         a.r = c;
         b.l = d;
         b.r = e;
         c.l = f;
         c.r = g;
         d.l = h;
-        
-        // INSERT NEW ELEMENT
-        a = t.insert(a, 25);   // inserting 25
-        
-        // SEARCH
-        System.out.println(t.searchinBST(25, a));  // true
+
+        System.out.println("Search 70: " + t.searchinBST(70, a));
+
+        // print inorder
+        System.out.print("INORDER: ");
+        t.inorder(a);
     }
 }
